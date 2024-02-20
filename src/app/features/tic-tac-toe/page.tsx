23c5a595c4438701b2a0084d70a48810e7938cee
Jess Bennett <jess.bennett@simplyhealth.co.uk>
2024-02-20 15:56:26 +0000

    feat: add rewind function

--- src/app/features/tic-tac-toe/page.tsx
+++ src/app/features/tic-tac-toe/page.tsx
@@ -1,25 +1,57 @@
 "use client";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
+import { LuSkipBack } from "react-icons/lu";
+import { MdOutlineSettingsBackupRestore } from "react-icons/md";
 
 import Board from "@/src/components/Board";
 
 const Game = () => {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: Array<string>) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(nextMove: SetStateAction<number>) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = (
+        <>
+          {<LuSkipBack />} {move}
+        </>
+      );
+    } else {
+      description = <MdOutlineSettingsBackupRestore />;
+    }
+    return (
+      <li key={move}>
+        <button className="c-btn" onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      </li>
+    );
+  });
+  console.log(moves);
+
   return (
     <div className="c-entry-card">
-      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />;
-      <ol></ol>
+      <Board
+        xIsNext={xIsNext}
+        squares={currentSquares}
+        onPlay={handlePlay}
+        moves={moves}
+      />
     </div>
   );
 };
 
 export default Game;
-
-//TODO: https://react.dev/learn/tutorial-tic-tac-toe#adding-time-travel
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -8,7 +8,8 @@ const Board: React.FC<{
   xIsNext: boolean;
   squares: Array<string>;
   onPlay: (a: Array<string>) => void;
-}> = ({ xIsNext, squares, onPlay }) => {
+  moves: JSX.Element[];
+}> = ({ xIsNext, squares, onPlay, moves }) => {
   const handleClick = (i: number) => {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -33,21 +34,24 @@ const Board: React.FC<{
     <>
       <h2 className="c-entry-card__title">{status}</h2>
       <div className="c-entry-card__content c-entry-card__content--board">
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <div>
+          <div className="board-row">
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
         </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
+        <ol>{moves}</ol>
       </div>
     </>
   );
